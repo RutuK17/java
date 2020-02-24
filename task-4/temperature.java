29fb2dac4dc351f3ccd3f79d59bc065ffd468d4a
rutuja kimmatkar <rutujakimmatkar@rutuja.geomitra.local>
2020-02-24 09:52:54 +0530

    print prime numbers

---
+++ task-4/temperature.java
@@ -0,0 +1,24 @@
+class conversion{
+    public int conv(int f, int c) {
+        for(int f=20; f < 300; f=f+20){
+            int c = ((f-32)*5)/9;
+            System.out.println(f+" "+"  "+c);
+        }
+        
+    }
+} 
+    
+
+class temperature{
+    public static void main(String[] args) {
+
+        int c,f;
+        f = 1;
+        c = ((f-32)*5)/9;
+        System.out.println(f+" "+"  "+c);
+         
+    }
+
+    
+    }
+
